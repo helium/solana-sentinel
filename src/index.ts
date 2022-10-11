512b770066608f1445517948e1e756df5a091630
Andrew Allen <allenan@helium.com>
2022-10-11 14:29:03 -0400

    force ssl in production

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,25 @@ import { statusRouter } from './status.router'
 dotenv.config()
 
 const PORT = parseInt(process.env.PORT || '3000', 10)
+const ENV = process.env.NODE_ENV || 'development'
 
 const app = express()
+
+if (ENV === 'production') {
+  app.enable('trust proxy')
+
+  // Enforce SSL & HSTS in production
+  app.use((req, res, nextPlug) => {
+    const proto = req.headers['x-forwarded-proto']
+    if (proto === 'https') {
+      res.set({
+        'Strict-Transport-Security': 'max-age=31557600', // one-year
+      })
+      return nextPlug()
+    }
+    res.redirect(`https://${req.headers.host}${req.url}`)
+  })
+}
 
 app.use(helmet())
 app.use(cors())
